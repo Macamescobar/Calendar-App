1114904e408e04a65b1145fb7191c1174ec85b30
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2023-03-02 12:22:49 -0300

    add nav bar and calendar

---
+++ src/calendar/components/Navbar.jsx
@@ -0,0 +1,19 @@
+
+export const NavBar = () => {
+  return (
+    <div
+      className="navbar navbar-dark bg-dark px-4"
+      style={{ width: "100vw", marginTop: "-3rem"}}
+    >
+      <span className="navbar-brand">
+        <i className="fas fa-calendar-alt"></i>
+        &nbsp; Macarena
+      </span>
+
+      <button className="btn btn-outline-danger">
+        <i className="fas fa-sign-out-alt"></i>
+        <span>Salir</span>
+      </button>
+    </div>
+  );
+};
