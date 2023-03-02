cf4486deeda94294950b453112d4d7ae6d057661
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2023-03-02 18:04:48 -0300

    add events

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,46 +1,77 @@
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { Calendar } from "react-big-calendar";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
-import enUS from 'date-fns/locale/en-US';
+import { addHours } from "date-fns";
 
-import { NavBar } from "../";
+import { NavBar, CalendarEvent } from "../";
+import { localizer, messages } from "../../helpers";
+import { useState } from "react";
 
-const locales = {
-  "en-US": enUS,
-};
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-});
 
 const events = [{
   title: 'Cumpleaños del jefe',
   notes: 'Hay que comprar el pastel',
   start: new Date(),
   end: addHours(new Date(), 2),
-  bgColor: "#fafafa",
+  backgroundColor: 'linear-gradient(to top, #9890e3 0%, #b1f4cf 100%)',
   user: {
     _id: '123',
-    name: 'Fernando'
+    name: 'Macarena'
   }
 }]
 
 export const CalendarPage = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week')
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    //console.log({event, start, end, isSelected});
+
+    const style = {
+      background: '#835af1',
+      borderRadius: 4,
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  }
+
+  const onDoubleClick = ( event ) => {
+    console.log({ doubleClick: event })
+  }
+
+  const onSelect = ( event ) => {
+    console.log({ click: event })
+  }
+
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event);
+    setLastView( event );
+  }
+
   return (
     <>
       <NavBar />
 
       <Calendar
-        localizer={localizer}
+        localizer={ localizer }
         events={ events }
+        defaultView={ lastView }
         startAccessor="start"
         endAccessor="end"
         style={{ height: "80vh", marginTop: "3rem" }}
+        messages={ messages()}
+        eventPropGetter={ eventStyleGetter }
+        components={{
+          event: CalendarEvent
+        }}
+        onDoubleClickEvent={ onDoubleClick }
+        onSelectEvent = { onSelect }
+        onView = { onViewChanged }
       />
     </>
   );
